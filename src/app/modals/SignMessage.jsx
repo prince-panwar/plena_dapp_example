39330760821c4edf0499e9837b1cdce7f4f0fd85
Prince <princegujjar1.pp@gmail.com>
2024-01-09 18:23:12 +0530

    formating

--- src/app/modals/SignMessage.jsx
+++ src/app/modals/SignMessage.jsx
@@ -1,9 +1,6 @@
-'use Client'
-import React, from 'react';
-import { Modal, ModalContent,  ModalBody, ModalFooter, Spinner,Button } from "@nextui-org/react";
-
+import React from 'react';
+import { Modal, ModalContent, ModalBody, ModalFooter, Spinner, Button } from "@nextui-org/react";
 import { shortAddress } from '../utils/shortAddress';
-
 
 export default function SignMessageModal({
   isModalOpen,
@@ -11,62 +8,60 @@ export default function SignMessageModal({
   pendingRequest = false,
   result = {},
 }) {
+  console.log(pendingRequest);
 
   return (
-<>
-
- <Modal isOpen={isModalOpen} closeButton={true} isDismissable={true}  onClose={onCancel} >
+    <>
+      <Modal isOpen={isModalOpen} closeButton={true} isDismissable={true} onClose={onCancel}>
         <ModalContent>
           {(onClose) => (
             <>
-              
               <ModalBody>
-              {pendingRequest ? (
-          <>
-            <p className='font-bold text-xl text-black-500'>Pending Request</p>
-            <Spinner label="Loading..." color="warning"/>
-            <p className='font-medium text-lg text-gray-500'>
-              Accept or Reject request using your wallet
-            </p>
-          </>
-        ) : result ? (
-          <>
-            <p className='font-bold text-xl text-black-500'>Request Approved</p>
-            <div className='w-full'>
-              <div className='flex items-center'>
-                <p className='text-base font-bold w-20'>method:</p>
-                <p className='text-md font-ligh'>{result?.method}</p>
-              </div>
-              <div className='flex items-center'>
-                <p className='text-base font-bold w-20'>signature:</p>
-                <p className='text-md font-ligh'>
-                  {shortAddress(result?.signature, 8)}
-                </p>
-              </div>
-              <div className='flex items-center'>
-                <p className='text-base font-bold w-20'>From:</p>
-                <p className=' text-xs font-ligh'>{result?.from}</p>
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <p className='font-bold text-3xl text-black-500'>
-              Request Rejected
-            </p>
-          </>
-        )}
+                {pendingRequest ? (
+                  <>
+                    <p className='font-bold text-xl text-black-500'>Pending Request</p>
+                    <Spinner label="Loading..." color="warning"/>
+                    <p className='font-medium text-lg text-gray-500'>
+                      Accept or Reject request using your wallet
+                    </p>
+                  </>
+                ) : result ? (
+                  <>
+                    <p className='font-bold text-xl text-black-500'>Request Approved</p>
+                    <div className='w-full'>
+                      <div className='flex items-center'>
+                        <p className='text-base font-bold w-20'>method:</p>
+                        <p className='text-md font-ligh'>{result?.method}</p>
+                      </div>
+                      <div className='flex items-center'>
+                        <p className='text-base font-bold w-20'>signature:</p>
+                        <p className='text-md font-ligh'>
+                          {shortAddress(result?.signature, 8)}
+                        </p>
+                      </div>
+                      <div className='flex items-center'>
+                        <p className='text-base font-bold w-20'>From:</p>
+                        <p className='text-xs font-ligh'>{result?.from}</p>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <p className='font-bold text-3xl text-black-500'>
+                      Request Rejected
+                    </p>
+                  </>
+                )}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onCancel}>
                   Close
                 </Button>
-              
               </ModalFooter>
             </>
           )}
         </ModalContent>
       </Modal>
-       </>
+    </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
-"use client"
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { eip1271 } from './utils/eip1271';
 import { hashMessage } from './utils/utiliities';
 import { Button } from '@nextui-org/react';
@@ -7,15 +6,15 @@ import { usePlenaWallet } from 'plena-wallet-sdk';
 import ThemeSwitcher from './components/theme.Switcher';
 import SendTxnModal from './modals/SendTxnModal';
 import SignMessageModal from './modals/SignMessage';
-
 import { ethers } from 'ethers';
 import Header from './components/Header';
+
 export default function Home() {
   const [pending, setPending] = useState(false);
   const [isSignModalOpen, setIsSignModalOpen] = useState(false);
   const [isTxnModalOpen, setIsTxnModalOpen] = useState(false);
   const [result, setResult] = useState(null);
-  const { openModal, closeConnection, sendTransaction, walletAddress } =usePlenaWallet();
+  const { openModal, closeConnection, sendTransaction, walletAddress } = usePlenaWallet();
 
   const disconnect = async () => {
     closeConnection();
@@ -40,6 +39,7 @@ export default function Home() {
     setPending(false);
     setResult(false);
   };
+
   const cancelTransaction = () => {};
 
   const testSendTransaction = async () => {
@@ -76,10 +76,7 @@ export default function Home() {
     ];
     const contract = new ethers.utils.Interface(abi1);
 
-    const txnData1 = contract.encodeFunctionData('transfer', [
-      lendingPool,
-      '1',
-    ]);
+    const txnData1 = contract.encodeFunctionData('transfer', [lendingPool, '1']);
     // Draft transaction
     const tx = {
       from: walletAddress,
@@ -88,6 +85,7 @@ export default function Home() {
       tokens: ['', ''],
       amounts: ['0x0', '0x0'],
     };
+
     try {
       const res = await sendTransaction({
         chain: 137,
@@ -97,10 +95,12 @@ export default function Home() {
           // todo: payload of transaction
         },
       });
+
       if (!res?.success) {
         setResult(false);
         return;
       }
+
       const formattedResult = {
         method: 'send_transaction',
         txHash: res?.content?.transactionHash,
@@ -117,6 +117,7 @@ export default function Home() {
   const testSignTransaction = async () => {
     openSignModal();
     setPending(true);
+
     try {
       const message = `My email is john@doe.com - ${new Date().toUTCString()}`;
 
@@ -132,20 +133,21 @@ export default function Home() {
           msgParams,
         },
       });
+
       if (!res?.success) {
         setResult(false);
         return;
       }
+
       const hash = hashMessage(message);
-      const polygonProvider = new ethers.providers.JsonRpcProvider(
-        'https://polygon-rpc.com/'
-      );
+      const polygonProvider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/');
       const valid = await eip1271.isValidSignature(
         walletAddress,
         res?.content?.signature,
         hash,
         polygonProvider
       );
+
       const formattedResult = {
         method: 'personal_sign',
         signature: res?.content?.signature,
@@ -158,51 +160,52 @@ export default function Home() {
       setPending(false);
     }
   };
-  
-  
+
   return (
     <>
-    <ThemeSwitcher/>
-    {!walletAddress ? (
-      <>
-        <div className='flex flex-col items-center justify-center h-screen'>
-          <h1 className='mb-8 text-2xl font-bold'>Welcome to Plena Connect</h1>
-          <Button
-            radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" onClick={openModal}>
-            Connect
-          </Button>
-        </div>
+      <ThemeSwitcher />
+      {!walletAddress ? (
+        <>
+          <div className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='mb-8 text-2xl font-bold'>Welcome to Plena Connect</h1>
+            <Button
+              radius='full'
+              className='bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg'
+              onClick={openModal}
+            >
+              Connect
+            </Button>
+          </div>
         </>
       ) : (
         <div className='h-100 flex justify-center items-center mt-8'>
-        <div className=' h-4/5 w-72 border-3 border-solid border-[#985AFF] rounded-2xl'>
-          <Header disconnect={disconnect} walletAddress={walletAddress} />
-          <div className='flex flex-col items-center justify-center '>
-            <h1 className='mb-8 text-2xl  mt-20'>Methods</h1>
-            <div className='flex flex-col h-auto'>
-              <Button
-                color='primary'
-                onClick={testSignTransaction}
-            >    Personal Sign
-              </Button>
-              <Button
-              className='mt-8'
-                color='primary'
-                onClick={testSendTransaction}
-                cancelTransaction={cancelTransaction}>
-              
-                Send Transaction
-              </Button>
-              <Button
-              color='primary'
-              variant='bordered'
-              onClick={disconnect}
-              className='  mb-8 mx-2   mt-40 px-10 py-5 f'>
-                Disconnect
-              </Button>
+          <div className=' h-4/5 w-72 border-3 border-solid border-[#985AFF] rounded-2xl'>
+            <Header disconnect={disconnect} walletAddress={walletAddress} />
+            <div className='flex flex-col items-center justify-center '>
+              <h1 className='mb-8 text-2xl mt-20'>Methods</h1>
+              <div className='flex flex-col h-auto'>
+                <Button color='primary' onClick={testSignTransaction}>
+                  Personal Sign
+                </Button>
+                <Button
+                  className='mt-8'
+                  color='primary'
+                  onClick={testSendTransaction}
+                  cancelTransaction={cancelTransaction}
+                >
+                  Send Transaction
+                </Button>
+                <Button
+                  color='primary'
+                  variant='bordered'
+                  onClick={disconnect}
+                  className='mb-8 mx-2 mt-40 px-10 py-5 f'
+                >
+                  Disconnect
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       )}
       <SendTxnModal
@@ -218,5 +221,5 @@ export default function Home() {
         result={result}
       />
     </>
-  )
+  );
 }
