79a350e86cca79f3fbb8cd15a095c8a105ad3cf4
Prince Panwar <155619078+Plena-Prince@users.noreply.github.com>
2024-01-10 12:05:42 +0000

    Normal layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,22 @@
 "use client";
-import dynamic from "next/dynamic";
 import "./globals.css";
-const DynmaicLayout = dynamic(() => import("./index"), {
-  ssr: false,
-});
-export default function RootLayout({ children }) {
+import { UIProviders } from "./providers/UIProvider";
+import { PlenaWalletProvider } from "plena-wallet-sdk";
+
+const config = {
+  dappToken:
+    "91651531fc0ff6f89808b72c7ca0fcda6a9816a225e33f4b226e5bfdadccf776007dee0a61aa8bfd8f32ceed5c3374da4b820f51b1dd1829c441aaa4eee83891",
+  dappId: "cm61ds5dotv8m80olbig",
+  bridgeUrl: "connect.plena.finance",
+};
+
+export default function MainLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <DynmaicLayout>{children}</DynmaicLayout>
+        <PlenaWalletProvider config={config}>
+          <UIProviders>{children}</UIProviders>
+        </PlenaWalletProvider>
       </body>
     </html>
   );
